c79cb1632d023f85d27ce1c6b1f0f47e2f68c54b
panda <347038217@qq.com>
2025-03-28 15:36:32 +0800

    update

---
+++ src/models/wechat_robot_online/api/main_api.py
@@ -0,0 +1,165 @@
+
+import datetime
+import sys
+import time
+sys.path.append("./src")
+import requests
+
+from models.wechat_robot_online.types.robot_task_type import RobotTask
+
+
+from models.wechat_robot_online.types.log_processing_type import LogProcessing
+from utils import local_logger
+from utils.table_image import create_table_image
+
+
+import pandas as pd
+from models.wechat_robot_online.types.organization_group_type import OrganizationGroup
+from models.wechat_robot_online.types.vehicle_type import Vehicle
+
+from utils.download_file import download_excel_and_read
+
+
+def get_vehicles_from_url(excel_url:str) -> list[Vehicle]:
+    # 读取Excel文件
+    
+    df:pd.DataFrame = download_excel_and_read(excel_url)
+    
+    df['车牌号码'] = df['车牌号码'].fillna('').astype(str)
+    df['车辆组织'] = df['车辆组织'].fillna('').astype(str)
+    # 类型为时间
+    df['车辆状态（离线/定位）'] = df['车辆状态（离线/定位）'].fillna('').astype(str)
+    df['摄像头状态'] = df['摄像头状态'].fillna('').astype(str)
+    if df.get('服务到期时间') is not None:
+        df['服务到期时间'] = df['服务到期时间'].fillna('').astype(str)
+    else:
+        df['服务到期时间'] = ''
+    # print(df)
+    # print("asdasddasd  ",df)
+    if df is None:
+        return None
+    # 将Excel数据转化为Vehicle对象列表
+    
+    vehicles:list[Vehicle] = []
+    for index, row in df.iterrows():
+        plate_number = row['车牌号码']
+        organization = row['车辆组织']
+        status:str = row['车辆状态（离线/定位）']
+        camera_status = row['摄像头状态']
+        # row['服务到期时间']
+        expiration_date = row['服务到期时间']
+        print("expiration_date: ", expiration_date)
+        
+        # print("status: ", status , isinstance(status, float))
+        # 判断status 是否为浮点数
+        try:
+            # print("status is float")
+            # 假设从Excel中读取的时间值是 "0.0770833333333333"（1小时51分钟）
+            excel_time = float(status)
+            print("excel_time: ", excel_time)
+            hours = int(excel_time * 24)  # 将小数部分转换为小时数
+            print("hours: ", hours)
+            # 将Excel时间值转换为Python的datetime对象
+            minutes = int((excel_time * 24- hours)  * 60)
+            print("minutes: ", minutes)
+            my_datetime = datetime.datetime(1900, 1, 1, hours, minutes)  # 日期部分可以是任意日期
+            print("my_datetime: ", my_datetime)
+            # 将datetime对象格式化为字符串
+            time_str = my_datetime.strftime("%H:%M")
+            print("time_str: ", time_str)
+            status = time_str
+        except Exception as e:
+            pass 
+            # print("status is not float" , e)
+            pass
+        
+        vehicle = Vehicle(plate_number, organization, status, camera_status,expiration_date)
+        # print("vehicle: ", vehicle.__str__())
+        vehicles.append(vehicle)
+    return vehicles
+
+
+def get_organizationgroups_from_url(excel_url: str) -> list[OrganizationGroup]:
+
+    # 使用download_excel_and_read函数下载Excel文件并读取内容
+    df = download_excel_and_read(excel_url)
+    
+    if df is not None:
+        # 创建一个空列表来存储OrganizationGroup对象
+        organization_groups = []
+
+        # 遍历DataFrame的行，为每一行的数据创建一个OrganizationGroup对象
+        for index, row in df.iterrows():
+            organization = row['车辆组织']
+            group_name = row['微信服务群名称']
+            vehicle_status_speech = row['车辆状态（离线/定位）']
+            camera_status_speech = row['摄像头状态']
+            org_group = OrganizationGroup(organization, group_name , vehicle_status_speech , camera_status_speech)
+            organization_groups.append(org_group)
+            
+        return organization_groups
+    else:
+        return []
+
+def get_log_processing(vehicle_url, organization_group_url) -> LogProcessing:
+    vehicle_list = get_vehicles_from_url(vehicle_url)
+    org_group_list = get_organizationgroups_from_url(organization_group_url)
+    
+    log_processing = LogProcessing(vehicle_list, org_group_list)
+    return log_processing
+    pass
+
+def get_tasks( vehicle_url, organization_group_url) -> list[RobotTask]:
+    log_processing = get_log_processing(vehicle_url, organization_group_url)
+    tasks = log_processing.get_all_robot_task_by_group_and_status()
+    return tasks
+    pass 
+
+def upload_img_file(img_path:str) -> None:
+    url = 'http://47.116.201.99:8001/test/upload_file'
+    file_path = img_path
+    # files = {'file': ( , open(file_path, 'rb'))}
+    files = {'file': open(file_path, 'rb')}
+    response = requests.post(url, files=files)
+    
+    # 检查响应状态码是否为 200，表示请求成功
+    if response.status_code == 200:
+        # 使用 response.json() 方法解析返回的 JSON 数据
+        json_data = response.json()
+        
+        # 获取 file_url 字段的值
+        file_url = json_data.get('file_url', None)
+        
+        if file_url:
+            return file_url
+        else:
+            print("File URL not found in the JSON response.")
+            return None
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+        return None
+
+    
+
+if __name__=='__main__':
+
+    # 传入Excel文件路径，并获取OrganizationGroup对象列表
+    # vehicle_url= "http://127.0.0.1:8001/test/uploads/5d1cf85c71b94aa4bdac16393c986d55_12.6.xlsx"
+    # excel_file_path= "http://localhost:8001/test/uploads/757e28eb783e4f86af3e48bb2580c237_-12-6.xlsx"
+    
+    vehicle_url= "http://0.0.0.0:25432/FileStoreAPI/get_file_uu_id/10bc559c-8680-4ba2-815f-7fd1b0264359"
+    # a71a109e-bb4c-4efb-a8dc-d44f3a678ce4
+    excel_file_path= "http://0.0.0.0:25432/FileStoreAPI/get_file_uu_id/7e985b48-a5fe-49e1-b225-d68b3b769f54"
+
+
+    # 获取分类后的数据
+    # 创建LogProcessingType对象并进行分类
+    log_processing = get_log_processing(vehicle_url, excel_file_path)
+    
+    tasks = log_processing.get_all_robot_task_by_group_and_status()
+    tasks.extend(log_processing.get_all_robot_task_by_group())
+    print(len(log_processing.vehicle_data_by_group.keys()) , len(tasks) )
+    for task in tasks:
+        print("task: ", task.task_type,task.to_user , task.content)
+
+    # Create a DataFrame from the list of dictionaries
---
+++ src/utils/table_image.py
@@ -0,0 +1,133 @@
+import os
+import uuid
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+import platform
+import textwrap
+
+# 获取当前操作系统的名称
+current_os = platform.system()
+
+# 打印操作系统名称
+print("当前操作系统:", current_os)
+
+
+matplotlib.use('Agg')
+
+def wrap_text(text, width):
+    """
+    Wrap text to a specified width.
+
+    Parameters:
+    text (str): The text to wrap.
+    width (int): The maximum line width in characters.
+
+    Returns:
+    str: The wrapped text.
+    """
+    return textwrap.fill(text, width)
+
+def create_table_image(df,title:str = "", directory = "./data/pngs/", file_name=None, base_height_per_row=0.25, base_width_per_column=2.8, min_width=5, min_height=4, max_width=30, max_height=250, dpi=300):
+    """
+    Create an image of a pandas DataFrame as a table, save it in the specified directory, and return the file path.
+
+    Parameters:
+    df (pandas.DataFrame): DataFrame to be visualized.
+    directory (str): Directory where the image will be saved.
+    file_name (str): Name of the file to save the image.
+    base_height_per_row (float): Base height per row in inches.
+    base_width_per_column (float): Base width per column in inches.
+    min_width (int): Minimum width of the image in inches.
+    min_height (int): Minimum height of the image in inches.
+    max_width (int): Maximum width of the image in inches.
+    max_height (int): Maximum height of the image in inches.
+    dpi (int): Dots per inch (resolution of the image).
+
+    Returns:
+    str: Path of the saved image file.
+    """
+    if file_name is None:
+        # file_name = 'table_image.png'
+        # 获取一个随机的名字
+        file_name = uuid.uuid4().hex + '.png'
+        
+    # Replace "nan" values with empty strings
+    df = df.fillna("")
+    
+    # Copy the DataFrame to avoid modifying the original data
+    # df = df.copy()
+
+    # Apply text wrapping to the specified column
+    # df["Camera_Status"] = df["Camera_Status"].apply(lambda x: wrap_text(str(x), 100))
+
+
+    # 判断当前操作系统类型
+    if current_os == "Linux":
+        plt.rcParams['font.family'] = 'sans-serif'
+        plt.rcParams['font.sans-serif'] = ['AR PL UKai CN']
+    elif current_os == "Windows":
+        print("这是Windows操作系统")
+    elif current_os == "Darwin":
+        # Set font properties for displaying Chinese characters 
+        plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+        print("这是macOS操作系统")
+    else:
+        print("未知操作系统")
+
+
+    # Calculate the ideal image size
+    ideal_width = min(max(df.shape[1] * base_width_per_column, min_width), max_width)
+    ideal_height = min(max(df.shape[0] * base_height_per_row, min_height), max_height)
+
+    # Create a figure and axes with the calculated size
+    fig, ax = plt.subplots(figsize=(ideal_width, ideal_height))
+
+    # Plot the DataFrame as a table
+    table = ax.table(cellText=df.values, colLabels=df.columns,colWidths=[0.1,0.20,0.2,0.3,0.2], cellLoc='center', loc='center')
+
+    # Adjust table font size
+    table.auto_set_font_size(False)
+    table.set_fontsize(9)
+    
+    # Hide axes
+    ax.axis('off')
+    ax.axis('tight')
+    plt.title(title, fontsize=16)
+    # Adjust layout
+    plt.tight_layout()
+
+
+    # Ensure directory exists
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Full path for the image
+    full_path = os.path.join(directory, file_name)
+
+    # Save the figure as an image
+    plt.savefig(full_path, dpi=dpi)
+
+    # Return the full path of the file
+    return os.path.abspath(full_path)
+
+# Example usage
+# df = pd.read_excel("path_to_your_excel_file.xlsx", engine='openpyxl', nrows=20)
+# file_path = create_table_image(df, directory='/path/to/save/directory', file_name='your_output_file_name.png')
+# print("Image saved at:", file_path)
+
+
+
+
+# data = {
+#     '车牌号': ['苏B00666D', '苏B80997', '苏B99862', '苏BA9822', '苏BM0182', '苏BM0190', '苏BM2308', '苏BM8853', '苏BM8885', '苏BR5736', '苏BR8161', '苏BW3705', '苏BW5960', '苏BY2715'],
+#     '公司名称': ['无锡市绿洲接送客运服务有限公司'] * 14,
+#     '状态': ['离线1天', '离线1天', '离线2天', '熄火4天', 'nan', '熄火1天', '熄火1天', 'nan', 'nan', '熄火1天', '熄火1天', '熄火1天', '离线8天', '离线5天'],
+#     '问题描述': ['nan', 'nan', 'nan', 'nan', 'AV03,AV04遮挡', 'nan', 'nan', '通通道全黑屏', 'AV03摄像头需要擦拭', 'nan', 'nan', 'nan', 'nan', 'nan']
+# }
+if __name__ == '__main__':
+    # df = pd.read_excel("/Users/panda/Desktop/github.nosync/ticketing-website/backend/data/微信服务群规则-测试.xlsx", engine='openpyxl',nrows=20)
+    df = pd.read_excel("/Users/panda/Desktop/github.nosync/ticketing-website/backend/data/部标监控日志12.6.xlsx", engine='openpyxl',nrows=20)
+    
+    print(create_table_image(df) )
